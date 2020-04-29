c55a626076c3188b12d372f7829486e4246c8f0c
ignacio <ignacio.lopez.arzuaga@testrigor.com>
2020-04-28 23:26:25 -0300

    Project commit.

---
+++ music-detector-srv/src/main/java/com/musicinfofinder/musicdetectorsrv/MusicDetectorSrvApplication.java
@@ -0,0 +1,13 @@
+package com.musicinfofinder.musicdetectorsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDetectorSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicDetectorSrvApplication.class, args);
+	}
+
+}
---
+++ music-detector-srv/src/test/java/com/musicinfofinder/musicdetectorsrv/MusicDetectorSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicinfofinder.musicdetectorsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDetectorSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
